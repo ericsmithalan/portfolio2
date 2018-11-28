4892f091dbe487e12cbdb9ef14c8a3349edeb028
Eric Smith <heathornhawk@gmail.com>
2018-11-28 14:48:18 -0800

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "@core";
-import "@styles/index.css";
+import "@styles/_index.scss";
 import { ServiceWorker } from "@core";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
