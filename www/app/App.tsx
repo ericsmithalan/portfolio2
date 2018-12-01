5e821cb32b9785920b6dc83e5f316a6aea65421e
Eric Smith <heathornhawk@gmail.com>
2018-12-01 12:43:33 -0800

    updates

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -1,19 +1,19 @@
 import './app.css';
 
 import * as React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { HomePage } from '../pages/';
 
 export class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact={true} path="/" component={HomePage} />
+                    <Route path="/home" component={HomePage} />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
