7c6c02642c3c9c2eeac6dcfa43c2f3dffb86797b
Eric Smith <heathornhawk@gmail.com>
2018-12-01 12:37:39 -0800

    updates

---
+++ www/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class HomePage extends Component {
+    public render() {
+        return (
+            <div className="page">
+                <div>Home page</div>
+            </div>
+        );
+    }
+}
